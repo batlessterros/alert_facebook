e42093d3e83977e992843c9946ab5084600da582
Hugo BALLESTEROS <hballesteros@lexsi.com>
2015-09-28 22:38:55 +0200

    aa

---
+++ facebook_up.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+import requests
+import signal
+import time
+import sys
+
+while True:
+    sys.stdout.write("Still down at: "+time.strftime("%Hh%M"))
+    sys.stdout.flush()
+    fb_down = 0
+    try:
+        r = requests.get("https://www.facebook.com", timeout=1)
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except:
+        fb_down = 1
+        sys.stdout.write("\r")
+        continue
+
+    sys.stdout.write("\r")
+    if(r.status_code == 200):
+        sys.stdout.write("\r")
+        print("World has been saved!")
+        if(raw_input("Print what has been get? (y/N)\n") == "y"):
+            print("World has been saved!")
+            sys.stdout.write(r.text)
+        break
